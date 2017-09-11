c2d7ecd8cfeae41d44e2771125d78a3d7f0c0967
leeeunkyu <xdkyu01@gmail.com>
2017-09-12 00:36:55 +0900

    first branch

--- TestProject/src/TestGit.java
+++ TestProject/src/TestGit.java
@@ -1,10 +1,10 @@
 
 public class TestGit {
 	public static void main(String[] args) {
-		System.out.println("hello");
-		System.out.println("hello");
-		System.out.println("hello");
+		System.out.println("hello1");
+		System.out.println("hello2");
+		System.out.println("hello3");
+		System.out.println("hello branch");		
 
-		
 	}
 }
